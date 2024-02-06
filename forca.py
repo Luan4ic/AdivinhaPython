bb69b644cdd4fa7f97400691c202c35d70d6ae9d
Luan Carvalho <luandasilva1000@gmail.com>
2024-02-06 20:59:16 -0300

    first commit

---
+++ forca.py
@@ -0,0 +1,12 @@
+
+
+def jogar():
+    print("**********************************")
+    print("****Bem vindo ao jogo de Forca!***")
+    print("**********************************")
+
+    print("Fim do jogo. Obrigado por jogar!")
+
+if(__name__ == "__main__"):
+    jogar()
+
